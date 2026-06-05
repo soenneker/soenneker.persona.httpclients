18d6f4ff5c4abdc4973c431b403e56e34b35e0a3
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:33 -0500

    xml commenting

--- src/Soenneker.Persona.HttpClients/Abstract/IPersonaOpenApiHttpClient.cs
+++ src/Soenneker.Persona.HttpClients/Abstract/IPersonaOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Persona.HttpClients.Abstract;
 /// </summary>
 public interface IPersonaOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
